d6d2e540cca1bac7857890cd45ffcdb86e1d9efb
InternMarz <rdemarcus323@gmail.com>
2025-10-11 16:45:21 -0400

    video 3

--- Assets/_Scripts/Bumper.cs
+++ Assets/_Scripts/Bumper.cs
@@ -1,8 +1,23 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bumper : MonoBehaviour
 {
-    
+    public float bumpForce = 2.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("PlayerBall"))
+        {
+            Vector3 myCenter = transform.position;
+            Vector3 conractPoint = collision.GetContact(0).point;
+
+            myCenter.y = conractPoint.y;
+            Vector3 forceVector = conractPoint - myCenter;
+
+            Rigidbody rb = collision.rigidbody;
+            rb.AddForce(forceVector * bumpForce, ForceMode.Impulse);
+        }
+    }
 }
---
+++ Assets/_Scripts/Flipper.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public KeyCode keyToPress;
+    private HingeJoint hinge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyToPress))
+        {
+            hinge.useSpring = false;
+            hinge.useMotor = true;
+        }else
+        {
+            hinge.useMotor = false;
+            hinge.useSpring = true;
+
+        }
+    }
+}
