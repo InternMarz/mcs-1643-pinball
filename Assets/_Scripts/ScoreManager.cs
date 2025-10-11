b8469ecb079a72a9276ff2707b7aaaef79263a06
InternMarz <rdemarcus323@gmail.com>
2025-10-11 14:29:45 -0400

    video 1

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddScore(int points)
+    {
+        // score = score + points;
+        score += points;
+    }
+}
